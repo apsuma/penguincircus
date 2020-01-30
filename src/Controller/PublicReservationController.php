47c8c24b11a5175f492ca12b4ae05db4ea8f673c
apsuma <delphine.belet@gmail.com>
2020-01-30 15:58:13 +0100

    process user-reservation ok

---
+++ src/Controller/PublicReservationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Article;
+use App\Entity\Reservation;
+use App\Form\ReservationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class PublicReservationController extends AbstractController
+{
+
+    /**
+     * @Route("/public/mareservation", name="public_reservation", methods={"GET","POST"})
+     */
+    public function ResaForMyColony(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserInterface $user
+     ): Response {
+        $reservation = new Reservation();
+        $form = $this
+            -> createForm(ReservationType::class, $reservation)
+            -> remove('createdAt')
+            -> remove('user')
+        ;
+        $form -> handleRequest($request);
+
+        if ($form -> isSubmitted() && $form -> isValid()) {
+            $reservation -> setUser($user);
+            $entityManager -> persist($reservation);
+            $entityManager -> flush();
+            $this -> addFlash(
+                'success',
+                'Votre demande de réservation a bien été prise en compte. Penguin Circus la traite et revient vers vous !'
+            );
+            return $this -> redirectToRoute('home');
+        }
+        return $this->render('reservation/mareservation.html.twig', [
+            'reservation' => $reservation,
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,12 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use App\Entity\Reservation;
 use App\Entity\User;
-use Doctrine\DBAL\Types\DateTimeType;
+use App\Repository\ArticleRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,10 +18,24 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nbPlaceChild', NumberType::class)
-            ->add('nbPlaceAdult', NumberType::class)
+            ->add('nbPlaceChild', NumberType::class, [
+                'label'=> 'Nombre de places enfant:'
+            ])
+            ->add('nbPlaceAdult', NumberType::class, [
+                'label'=> 'Nombre de places adulte :'
+            ])
             ->add('createdAt', DateTimeType::class)
-            ->add('articles', CollectionType::class)
+            ->add('articles', EntityType::class, [
+                'label' => 'évènement que je réserve:',
+                'class' => Article::class,
+                'query_builder' => function(ArticleRepository $articleRepo) {
+                    return $articleRepo
+                        ->createQueryBuilder('a')
+                        ->where('a.resaOpen = true');
+                } ,
+                'choice_label' => 'subject',
+                'required' => true
+            ])
             ->add('user', EntityType::class, ['class' => User::class])
         ;
     }
