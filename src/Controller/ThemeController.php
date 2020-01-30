51ca4da1c8726e308d68dfbd0962f22c77b13c10
apsuma <delphine.belet@gmail.com>
2020-01-30 10:48:42 +0100

    mise en forme crud theme ok

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ThemeController extends AbstractController
 {
     /**
-     * @Route("/theme/{id}", name="index")
+     * @Route("/theme/{id}", name="theme_index")
      */
     public function index(Theme $theme):Response
     {
